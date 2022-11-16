6431dc469f84b85ded7f6c327d933127a8e3886b
olfa-ayari99 <akkari.amir23@gmail.com>
2022-11-16 20:52:02 +0100

    first commit

---
+++ src/Controller/OeuvreController.php
@@ -0,0 +1,135 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Oeuvre;
+use App\Form\Oeuvre1Type;
+use App\Repository\OeuvreRepository;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\String\Slugger\SluggerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
+
+#[Route('/oeuvre')]
+class OeuvreController extends AbstractController
+{
+ 
+    
+    #[Route('/', name: 'app_oeuvre_index', methods: ['GET'])]
+    public function index(OeuvreRepository $oeuvreRepository): Response
+    {
+        return $this->render('oeuvre/index.html.twig', [
+            'oeuvres' => $oeuvreRepository->findAll(),
+        ]);
+    }
+
+     
+    #[Route('/front', name: 'oeuvre_index_front', methods: ['GET'])]
+    public function oeuvreFront(OeuvreRepository $oeuvreRepository): Response
+    {
+        return $this->render('oeuvre/index_front.html.twig', [
+            'oeuvres' => $oeuvreRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/new', name: 'app_oeuvre_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, OeuvreRepository $oeuvreRepository, SluggerInterface $slugger): Response
+    {
+        $oeuvre = new Oeuvre();
+        $form = $this->createForm(Oeuvre1Type::class, $oeuvre);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $image = $form->get('image')->getData();
+
+            // this condition is needed because the 'brochure' field is not required
+            // so the PDF file must be processed only when a file is uploaded
+            if ($image) {
+                $originalFilename = pathinfo($image->getClientOriginalName(), PATHINFO_FILENAME);
+                $safeFilename = $slugger->slug($originalFilename);
+                $newFilename = $safeFilename.'-'.uniqid().'.'.$image->guessExtension();
+                try {
+                    $image->move(
+                        $this->getParameter('oeuvres_directory'),
+                        $newFilename
+                    );
+                } catch (FileException $e) {
+                    // ... handle exception if something happens during file upload
+                }
+
+                // updates the 'brochureFilename' property to store the PDF file name
+                // instead of its contents
+                $oeuvre->setImage($newFilename);
+            }
+            $oeuvreRepository->save($oeuvre, true);
+
+            $this->addFlash('success_message' , 'Oeuvre ajoutée avec succés');
+            return $this->redirectToRoute('app_oeuvre_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('oeuvre/new.html.twig', [
+            'oeuvre' => $oeuvre,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_oeuvre_show', methods: ['GET'])]
+    public function show(Oeuvre $oeuvre): Response
+    {
+        return $this->render('oeuvre/show.html.twig', [
+            'oeuvre' => $oeuvre,
+        ]);
+    }
+
+    #[Route('/{id}/edit', name: 'app_oeuvre_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, Oeuvre $oeuvre, OeuvreRepository $oeuvreRepository , SluggerInterface $slugger): Response
+    {
+        $form = $this->createForm(Oeuvre1Type::class, $oeuvre);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $image = $form->get('image')->getData();
+
+            // this condition is needed because the 'brochure' field is not required
+            // so the PDF file must be processed only when a file is uploaded
+            if ($image) {
+                $originalFilename = pathinfo($image->getClientOriginalName(), PATHINFO_FILENAME);
+                $safeFilename = $slugger->slug($originalFilename);
+                $newFilename = $safeFilename.'-'.uniqid().'.'.$image->guessExtension();
+                try {
+                    $image->move(
+                        $this->getParameter('oeuvres_directory'),
+                        $newFilename
+                    );
+                } catch (FileException $e) {
+                    // ... handle exception if something happens during file upload
+                }
+
+                // updates the 'brochureFilename' property to store the PDF file name
+                // instead of its contents
+                $oeuvre->setImage($newFilename);
+            }
+            $oeuvreRepository->save($oeuvre, true);
+            $this->addFlash('success_message' , 'Oeuvre modifiée avec succés');
+            return $this->redirectToRoute('app_oeuvre_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('oeuvre/edit.html.twig', [
+            'oeuvre' => $oeuvre,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/delete/{id}', name: 'app_oeuvre_delete')]
+    public function delete(Request $request, Oeuvre $oeuvre, OeuvreRepository $oeuvreRepository): Response
+    {
+        $oeuvreRepository->remove($oeuvre, true);
+        $this->addFlash('success_message' , 'Oeuvre supprimée avec succés');
+        return $this->redirectToRoute('app_oeuvre_index', [], Response::HTTP_SEE_OTHER);
+    }
+
+   
+}
